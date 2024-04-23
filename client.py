3140fa18cde61da9f1ec905fa2d32f73570393de
ivanlukomskiy <ivanlukomskiy@avride.ai>
2024-04-23 14:13:51 +0300

    pid & stuff

--- client.py
+++ client.py
@@ -15,20 +15,40 @@ pygame.joystick.init()
 # Check for connected joysticks
 joystick_count = pygame.joystick.get_count()
 
-pi_ip = "192.168.178.193"
+pi_ip = "192.168.227.193"
 port = 9999
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 arm_max_angle = 180.
 neck_max_angle = 175.
 neck_min_angle = 5.
-plate_min_angle = 45.
-plate_max_angle = 135.
+plate_min_angle = 0.
+plate_max_angle = 180.
 delay_ms = 6
+
+
+class SimplePidController:
+    def __init__(self, value_min, value_max, kp, kd):
+        self.value_min = value_min
+        self.value_max = value_max
+        self.kp = kp
+        self.kd = kd
+        self.value = (value_max + value_min) / 2
+        self.prev_error = 0
+
+    def apply(self, target):
+        error = target - self.value
+        derivative = error - self.prev_error
+        output = self.value + (self.kp * error) - (self.kd * derivative)
+        print(f"value {self.value:.3f} target {target:.3f}, error {error:.3f} -> {(self.kp * error):.3f}, derivative {derivative:.3f}->{-self.kd * derivative:.3f}, diff: {(self.kp * error) - (self.kd * derivative):.3f}, output: {output:.3f}")
+        self.prev_error = error
+        self.value = max(self.value_min, min(output, self.value_max))
+        return self.value
 
 
 def clamp(value, min_value, max_value):
     return max(min(value, max_value), min_value)
+
 
 if joystick_count == 0:
     raise Exception("No joystick detected.")
@@ -41,6 +61,10 @@ else:
     print("Joystick Name:", joystick.get_name())
     print("Number of Axes:", joystick.get_numaxes())
 
+    plate_controller = SimplePidController(plate_min_angle, plate_max_angle, .09, .1)
+    neck_controller = SimplePidController(neck_min_angle, neck_max_angle, .02, .1)
+    left_arm_controller = SimplePidController(0., arm_max_angle, .04, .1)
+    right_arm_controller = SimplePidController(0., arm_max_angle, .04, .1)
 
     try:
         # Main loop to read joystick input
@@ -61,21 +85,21 @@ else:
             right_y = -positions[3]
 
             left_angle = math.atan2(left_y, left_x)
-            left_distance = min(math.sqrt(left_x**2 + left_y**2), 1.)
+            left_distance = min(math.sqrt(left_x ** 2 + left_y ** 2), 1.)
             right_angle = math.atan2(right_y, right_x)
-            right_distance = min(math.sqrt(right_x**2 + right_y**2), 1.)
+            right_distance = min(math.sqrt(right_x ** 2 + right_y ** 2), 1.)
 
             left_arm_max = 0
             right_arm_max = 0
-            if math.pi/2 <= left_angle < math.pi:
+            if math.pi / 2 <= left_angle < math.pi:
                 left_arm_max = 1.
                 right_arm_max = 1 - (left_angle - math.pi / 2) * 2 / math.pi
             elif 0 <= left_angle < math.pi / 2:
                 left_arm_max = left_angle * 2 / math.pi
                 right_arm_max = 1.
-            elif -math.pi/2 <= left_angle < 0:
+            elif -math.pi / 2 <= left_angle < 0:
                 left_arm_max = 0
-                right_arm_max = 1+left_angle * 2 / math.pi
+                right_arm_max = 1 + left_angle * 2 / math.pi
             else:
                 left_arm_max = -1 - left_angle * 2 / math.pi
                 right_arm_max = 0
@@ -85,12 +109,17 @@ else:
             neck_angle = neck_min_angle + (neck_max_angle - neck_min_angle) * (clamp(right_y, -1, 1) + 1) / 2
             plate_angle = plate_min_angle + (plate_max_angle - plate_min_angle) * (clamp(right_x, -1, 1) + 1) / 2
 
-            data = {"left_arm": left_arm_angle, "right_arm": right_arm_angle, "neck": neck_angle, "plate": 90.}
+            plate_angle = plate_controller.apply(plate_angle)
+            neck_angle = neck_controller.apply(neck_angle)
+            left_arm_angle = left_arm_controller.apply(left_arm_angle)
+            right_arm_angle = right_arm_controller.apply(right_arm_angle)
+
+            data = {"left_arm": left_arm_angle, "right_arm": right_arm_angle, "neck": neck_angle, "plate": plate_angle}
             json_data = json.dumps(data)
             sock.sendto(json_data.encode(), (pi_ip, port))
 
             # print(f"left_angle: {left_angle}, left_distance: {left_distance}, right_angle: {right_angle}, right_distance: {right_distance}")
-            print(f"left_arm_angle: {left_arm_angle}, right_arm_angle: {right_arm_angle}")
+            # print(f"left_arm_angle: {left_arm_angle}, right_arm_angle: {right_arm_angle}")
             pygame.time.wait(delay_ms)
 
     except KeyboardInterrupt:
